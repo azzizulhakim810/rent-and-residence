c849b20ba204e295178063e90fbab7081b628907
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-05-25 18:45:54 +0600

    Working on the Nav & Footer

--- rent-and-residence-client/src/layout/Main.jsx
+++ rent-and-residence-client/src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar";
+import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
   return (
-    <div>
+    <div className="w-10/12 mx-auto ">
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
