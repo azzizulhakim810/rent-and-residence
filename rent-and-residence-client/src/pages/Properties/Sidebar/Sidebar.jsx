3950b16d1ad806864f4c83784735e259c1a24a2a
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-06-04 18:40:47 +0600

    Working on the sidebar in Properties

--- rent-and-residence-client/src/pages/Properties/Sidebar/Sidebar.jsx
+++ rent-and-residence-client/src/pages/Properties/Sidebar/Sidebar.jsx
@@ -1,16 +1,93 @@
 const Sidebar = () => {
   return (
-    <div>
-      <div className="shadow-lg">
-        <h1>Sidebar</h1>
+    <div className="flex flex-col gap-6">
+      <div className="shadow-sm  p-8 w-full rounded-md bg-white">
+        <label
+          htmlFor="propertyType"
+          className="block text-[16px] font-[700] font-Nunito tracking-wider text-gray-700 mb-2"
+        >
+          Change Currency
+        </label>
         <select
           defaultValue="USD"
-          className=" join-item select block w-full ms-3 mx-5 bg-transparent border-gray-300 border-[1px] rounded focus:outline-none focus:ring-0 focus:ring-gray-300 focus:border-[1px] text-[14px] text-C_DarkGray focus:text-C_DarkGray font-Nunito_Sans"
+          className=" join-item select block w-full bg-transparent border-gray-300 border-[1px] rounded-md focus:outline-none focus:ring-0 focus:ring-gray-300 focus:border-[1px] text-[14px] text-gray-500 focus:text-gray-500 font-Nunito_Sans"
         >
-          <option disabled={true}>USD</option>
+          <option disabled={true}>Choose Here</option>
+          <option>USD</option>
           <option>EUR</option>
           <option>GBP</option>
         </select>
+      </div>
+
+      <div className="shadow-sm  p-8 w-full rounded-md bg-white">
+        <label
+          htmlFor="propertyType"
+          className="block text-[16px] font-[700] font-Nunito tracking-wider text-gray-700 mb-2"
+        >
+          Change Measurement
+        </label>
+        <select
+          defaultValue="square feet - ft2"
+          className=" join-item select block w-full bg-transparent border-gray-300 border-[1px] rounded-md focus:outline-none focus:ring-0 focus:ring-gray-300 focus:border-[1px] text-[14px] text-gray-500 focus:text-gray-500 font-Nunito_Sans"
+        >
+          <option disabled={true}>Choose Here</option>
+          <option>square feet - ft2</option>
+          <option>square meters - m2</option>
+          <option>acres - ac</option>
+          <option>square yards - yd2</option>
+          <option>hectares - ha</option>
+        </select>
+      </div>
+
+      <div className="shadow-sm  p-8 w-full rounded-md bg-white">
+        <label
+          htmlFor="propertyType"
+          className="block text-[16px] font-[700] font-Nunito tracking-wider text-C_DarkGray mb-5"
+        >
+          Our Listings
+        </label>
+
+        <ul className="list bg-base-100 rounded-box">
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+
+          <span className="bg-gray-400/50 h-[0.5px] my-4"></span>
+
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+
+          <span className="bg-gray-400/50 h-[0.5px] my-4"></span>
+
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+
+          <span className="bg-gray-400/50 h-[0.5px] my-4"></span>
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+          <span className="bg-gray-400/50 h-[0.5px] my-4"></span>
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+          <span className="bg-gray-400/50 h-[0.5px] my-4"></span>
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+          <span className="bg-gray-400/50 h-[0.5px] my-4"></span>
+          <li className=" flex justify-between font-Nunito_Sans text-[15px] text-C_LightGray">
+            <h1 className="">Apartments</h1>
+            <p>(2)</p>
+          </li>
+        </ul>
       </div>
     </div>
   );
