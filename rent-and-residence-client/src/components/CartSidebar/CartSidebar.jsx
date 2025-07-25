fd8174961a5af9ffd657e4d86b6913b74c2e7e56
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-07-26 01:14:27 +0600

    Img, title & price shows in the cart sidebar, have delete icon to delete

--- rent-and-residence-client/src/components/CartSidebar/CartSidebar.jsx
+++ rent-and-residence-client/src/components/CartSidebar/CartSidebar.jsx
@@ -1,12 +1,27 @@
-import { useState, useEffect } from "react";
-import UseAxiosSecure from "../../hooks/UseAxiosSecure/UseAxiosSecure";
-
 const CartSidebar = ({ item }) => {
-  // const axiosSecure = UseAxiosSecure();
+  // Destructure Details from Property
+  const { title, price } = item || {};
 
-  // console.log(item);
+  const propImg = item?.images?.[0];
 
-  return <div>Hello</div>;
+  console.log(item);
+
+  return (
+    <nav className="flex flex-col gap-2">
+      <div className="flex justify-between items-center gap-3 pt-2">
+        <img className="w-[18%] rounded" src={propImg} alt="" />
+        <span className="w-full flex flex-col gap-1">
+          <h4 className="font-Nunito font-[700] text-C_gray text-[15px] leading-6">
+            {title}
+          </h4>
+          <p className=" text-C_purple font-Nunito_Sans font-[600] text-[15px] leading-6">
+            {price} €
+          </p>
+        </span>
+        <button className="btn">X</button>
+      </div>
+    </nav>
+  );
 };
 
 export default CartSidebar;
