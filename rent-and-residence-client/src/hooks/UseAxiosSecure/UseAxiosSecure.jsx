fe0f631f690c810fcd3636b3724948ee6248acb4
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-08-11 12:56:11 +0600

    Can't verify the token properly

--- rent-and-residence-client/src/hooks/UseAxiosSecure/UseAxiosSecure.jsx
+++ rent-and-residence-client/src/hooks/UseAxiosSecure/UseAxiosSecure.jsx
@@ -1,16 +1,21 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import UseAuth from "../UseAuth/UseAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5123",
 });
 
 const UseAxiosSecure = () => {
+  const { signOutUser } = UseAuth();
+  const navigate = useNavigate();
+  // Request interceptor to add authorization header for every secure call to api
   axiosSecure.interceptors.request.use(
     function (config) {
       // Do something before request is sent
-      // console.log("request stopped by interceptors");
-
       const token = localStorage.getItem("access-token");
+      console.log("request stopped by interceptors", token);
+      // console.log(token);
 
       config.headers.authorization = `Bearer ${token}`;
       return config;
@@ -20,6 +25,34 @@ const UseAxiosSecure = () => {
       return Promise.reject(err);
     }
   );
+
+  // Response interceptor (Intercepts 401 & 403 status)
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (err) => {
+      const status = err.res.status;
+      console.log("Status error", status);
+
+      // For 401 & 403 user to logOut
+
+      if (status == 401 || status == 403) {
+        await signOutUser()
+          .then(() => {
+            console.log("Sign Out Successfully");
+            // loading(false);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+
+        navigate("/");
+      }
+      return Promise.reject(err);
+    }
+  );
+
   return axiosSecure;
 };
 
