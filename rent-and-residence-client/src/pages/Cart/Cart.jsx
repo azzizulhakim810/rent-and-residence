ec9aaa020f38177a11eed85ca5b43e378d3c4513
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-08-22 01:51:09 +0600

    Working on Cart Page

--- rent-and-residence-client/src/pages/Cart/Cart.jsx
+++ rent-and-residence-client/src/pages/Cart/Cart.jsx
@@ -1,7 +1,10 @@
 const Cart = () => {
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className=" bg-white">
+      <div className="grid grid-cols-12 gap-10 w-11/12 mx-auto pt-1">
+        <h1 className="font-Nunito text-2xl font-[600] pb-2">Welcome</h1>
+        <h1 className="font-Nunito text-5xl font-[800]">Cart Page</h1>
+      </div>
     </div>
   );
 };
