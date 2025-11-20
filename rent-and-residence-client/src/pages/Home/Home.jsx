25c706358fd65ff94db5432f249eeba91e4b1c9a
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-11-20 22:40:36 +0600

    turn off testimonials for now on the home page

--- rent-and-residence-client/src/pages/Home/Home.jsx
+++ rent-and-residence-client/src/pages/Home/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
 
       <div className="w-10/12 mx-auto ">
         <Location />
-        <Testimonials />
+        {/* <Testimonials /> */}
       </div>
 
       {/* Comparison Popup  */}
